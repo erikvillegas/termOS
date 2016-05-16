6014a080258f82da2713f12cab7141e243e3ceaf
Erik Villegas <erikvillegas1@gmail.com>
2016-05-16 07:50:33 -0700

    Initial Commit

---
+++ termOS/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  termOS
+//
+//  Created by Erik Villegas on 5/16/16.
+//  Copyright © 2016 Erik Villegas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
